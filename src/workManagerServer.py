bf32bbb7f199100ae6ad1fc91580aa87329457a4
Tønnes Nygaard <tonnesfn@ifi.uio.no>
2018-06-16 12:54:22 +0200

    workManagerServer: Added running commands from file

--- src/workManagerServer.py
+++ src/workManagerServer.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
         print("WorkManagerServer menu:\n")
         print("  evo - run evolutionary experiments")
         print("  rand - run random search experiments")
+        print("  file - run commands from file")
         print("  exit - exit the program")
         print();
 
@@ -31,6 +32,20 @@ if __name__ == '__main__':
             for i in range(experimentNumber):
                 channel.basic_publish(exchange='', routing_key='commands', body='experiments ra 1 exit')
             print("  Sent {} random search jobs to the queue.\n".format(experimentNumber))
+        elif choice == "file":
+            print("    This command reads newline-separated commands from file and distributes these to the workers")
+            filename = input("      Filename? >")
+            experimentNumber = int(input("      How many of each experiment do you want to run? >"))
 
+            with open(filename) as f:
+                content = f.readlines()
+
+            content = [x.strip() for x in content]
+
+            for i in range(experimentNumber):
+                for line in content:
+                    channel.basic_publish(exchange='', routing_key='commands', body=line)
+
+            print("      Commands sent successfully\n")
         else:
             print("Unknown")
