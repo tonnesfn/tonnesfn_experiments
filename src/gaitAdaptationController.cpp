bbbd173c31da0cb867c6a221fb4dafc6f2baafc8
tonnesfn <tonnes.nygaard@gmail.com>
2019-07-01 13:59:44 +1000

    Added a sweep of frequencies

--- src/gaitAdaptationController.cpp
+++ src/gaitAdaptationController.cpp
@@ -34,8 +34,9 @@ int main(int argc, char **argv) {
     while(1 != 2) {
 
         printf("\nMenu:\n");
-        printf("  rz - Send lowLevelSplineGait::zeroHeight command\n");
-        printf("  rzf - Send faster lowLevelSplineGait::zeroHeight command\n");
+        printf("  rz - Send lowLevelSplineGait::zeroHeight\n");
+        printf("  rzf - Send faster lowLevelSplineGait::zeroHeight\n");
+        printf("  rzs - Sweep s[eed lowLevelSplineGait::zeroHeight\n");
         printf("> ");
 
         std::string choice;
@@ -49,16 +50,35 @@ int main(int argc, char **argv) {
             printf("  Sent gait configuration message with lowLevelSplineGait::zeroHeight individual\n");
 
         } else if (choice == "rzf") {
+            std::map<std::string, double> tmpIndividual = individuals_lowLevelSplineGait::zeroHeight;
+            tmpIndividual["frequency"] = 0.3;
+            updateGaitConfiguration("lowLevelSplineGait", tmpIndividual, gaitConfiguration_client);
 
+            printf("  Sent gait configuration message with faster lowLevelSplineGait::zeroHeight individual\n");
 
-            for (int i = 10; i < 50; i++){
-                std::map<std::string, double> tmpIndividual = individuals_lowLevelSplineGait::zeroHeight;
-                tmpIndividual["frequency"] = i / 100.0;
-                updateGaitConfiguration("lowLevelSplineGait", tmpIndividual, gaitConfiguration_client);
-                printf("%.2f\n", tmpIndividual["frequency"]);
+        } else if (choice == "rzs") {
+            for (int j = 0; j < 10; j++) {
+                for (int i = 10; i < 50; i++) {
+                    std::map<std::string, double> tmpIndividual = individuals_lowLevelSplineGait::zeroHeight;
+                    tmpIndividual["frequency"] = i / 100.0;
+                    updateGaitConfiguration("lowLevelSplineGait", tmpIndividual, gaitConfiguration_client);
+                    printf("%.2f\n", tmpIndividual["frequency"]);
 
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                }
 
-                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+                std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+
+                for (int i = 50; i >= 10; i--) {
+                    std::map<std::string, double> tmpIndividual = individuals_lowLevelSplineGait::zeroHeight;
+                    tmpIndividual["frequency"] = i / 100.0;
+                    updateGaitConfiguration("lowLevelSplineGait", tmpIndividual, gaitConfiguration_client);
+                    printf("%.2f\n", tmpIndividual["frequency"]);
+
+                    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                }
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(2000));
             }
 
             printf("  Sent gait configuration message with faster lowLevelSplineGait::zeroHeight individual\n");
