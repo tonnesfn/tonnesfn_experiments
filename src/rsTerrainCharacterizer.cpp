91cb7fd5f0b5662e8d14aacd973237f1917e8e8b
tonnesfn <tonnes.nygaard@gmail.com>
2019-07-05 13:36:14 +1000

    Added new rsTerrainCharacterizer node with opencv

---
+++ src/rsTerrainCharacterizer.cpp
@@ -0,0 +1,85 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+static const std::string OPENCV_WINDOW = "Image window";
+
+class ImageConverter
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+
+public:
+  ImageConverter()
+    : it_(nh_)
+  {
+    // Subscrive to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/camera/depth/image_rect_raw", 1,
+      &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+    cv::namedWindow(OPENCV_WINDOW);
+  }
+
+  ~ImageConverter()
+  {
+    cv::destroyWindow(OPENCV_WINDOW);
+  }
+
+  void depthToCV8UC1(const cv::Mat& float_img, cv::Mat& mono8_img){
+    //Process images
+    if(mono8_img.rows != float_img.rows || mono8_img.cols != float_img.cols){
+      mono8_img = cv::Mat(float_img.size(), CV_16UC1);}
+    cv::convertScaleAbs(float_img, mono8_img, 100, 0.0);
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      //cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_16UC1);
+      cv_ptr = cv_bridge::toCvCopy(msg);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    cv::Mat depth_float_img;
+    cv::convertScaleAbs(cv_ptr->image, depth_float_img, 0.03);
+    cv::applyColorMap(depth_float_img, depth_float_img, cv::COLORMAP_JET);
+//    depthToCV8UC1(depth_float_img, depth_mono8_img);
+
+    // Draw an example circle on the video stream
+    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+      //cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+      cv::circle(depth_float_img, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+
+//    cv::Mat cm_img0;
+//    cv::applyColorMap(depth_mono8_img, cm_img0, cv::COLORMAP_JET);
+
+    // Update GUI Window
+    //cv::imshow(OPENCV_WINDOW, depth_float_img);
+    cv::imshow(OPENCV_WINDOW, depth_float_img);
+    cv::waitKey(3);
+
+    // Output modified video stream
+    image_pub_.publish(cv_ptr->toImageMsg());
+  }
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "image_converter");
+  ImageConverter ic;
+  ros::spin();
+  return 0;
+}
